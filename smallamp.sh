e57e6ff315690a266b297c0e5ec4e4dbd3764ac7
mabdi <m.abdi@aut.ac.ir>
2021-04-09 11:39:52 +0200

    smallamp.sh added

---
+++ smallamp.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+source "${SMALLTALK_CI_HOME}/pharo/run.sh"
+
+getImageDirName(){
+   # https://stackoverflow.com/a/15137779/1016452
+   local resolved_image="$(resolve_path "${config_image:-${SMALLTALK_CI_IMAGE}}")"
+   echo ${resolved_image%/*}
+}
+
+
+pharoSmallAmpCommand(){
+   local args=$1
+   local resolved_vm="${config_vm:-${SMALLTALK_CI_VM}}"
+   local resolved_image="$(resolve_path "${config_image:-${SMALLTALK_CI_IMAGE}}")"
+   travis_wait "${resolved_vm}" "${resolved_image}" --no-default-preferences smallamp ${args}" 
+} 
+
+installSmallAmp(){
+   print_info "Installing SmallAmp in the image:"
+   pharo::run_script "
+    | smalltalkCI |
+    $(conditional_debug_halt)
+    [ Metacello new
+        baseline: 'SmallAmp';
+        repository: 'github://mabdi/small-amp/src';
+        onUpgrade: [ :ex | ex useIncoming ];
+        onConflictUseIncoming;
+        load ] on: Warning do: [ :w | w resume ].
+    
+    (smalltalkCI isHeadless or: [smalltalkCI promptToProceed])
+      ifTrue: [ smalltalkCI saveAndQuitImage ]
+  "
+}
+
+makeStatFile(){
+   print_info "Building stat files for: ${reponame}"
+   pharoSmallAmpCommand  "--stat=${reponame}"
+}
+
+runAmp(){
+   print_info "Running amplification for: ${reponame}"
+ 
+   input="${getImageDirName}/todo.txt"
+   while IFS= read -r line
+   do
+      pharo::run_script "SmallAmp initializeDefault testCase: ${line}; amplifyEval"
+   done < "$input"
+
+}
+
+smallAmpMain(){
+   print_info "smallAmp main started: ${reponame}"
+   installSmallAmp
+   makeStatFile
+   print_info "smallAmp main finished"
+}
+
+smallAmpMain "$@"
