502de659ed31b65cfba9507ec32e15756d13b494
Alexandre Bergel <abergel@dcc.uchile.cl>
2021-05-18 14:41:34 +0200

    Create runTest.sh

---
+++ scripts/runTest.sh
@@ -0,0 +1,19 @@
+curl -L https://get.pharo.org/64/alpha+vm | bash
+./pharo --headless Pharo.image ./scripts/runTest.st
+
+FILE=/tmp/result.txt
+if [ ! -f "$FILE" ]; then
+    echo "ERROR: $FILE does not exists!"
+    exit 1
+fi
+
+
+cat $FILE
+set RES = `grep ERROR $FILE`
+
+if [ -n "$RES" ]; then
+		exit 1
+else
+		echo "ALL TEST PASSED"
+		exit 0
+fi
