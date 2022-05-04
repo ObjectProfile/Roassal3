27409d38a5f4fc06b5fb760f3aa06c9065da3822
Milton Mamani Torres <akevalion@gmail.com>
2022-05-04 15:50:33 +0200

    Update getCoverage.sh

--- scripts/getCoverage.sh
+++ scripts/getCoverage.sh
@@ -1,4 +1,3 @@
-#curl -L https://get.pharo.org/64/alpha+vm | bash
-# pharo 11 not ready
-curl -L https://get.pharo.org/100+vm | bash
+curl -L https://get.pharo.org/64/alpha+vm | bash
+# curl -L https://get.pharo.org/100+vm | bash
 ./pharo --headless Pharo.image ./scripts/runCoverage.st
