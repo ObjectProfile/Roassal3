84021efec7e3d7201dc7e61d8a325d60f9c49684
Milton Mamani Torres <akevalion@gmail.com>
2022-11-18 14:54:29 +0100

    Update runTest.sh

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -3,7 +3,7 @@
 
 #curl -L https://get.pharo.org/100+vm | bash
 curl -L https://get.pharo.org/64/alpha+vm | bash
-
+echo ${REPO_NAME}
 yovi="/tmp/io.txt"
 echo ${GITHUB_BASE_REF} > $yovi
 echo ${GITHUB_REPOSITORY} >> $yovi
