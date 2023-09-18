f3748ab8506ab7ea1bafa832df531c999e6dcafb
Milton Mamani Torres <akevalion@gmail.com>
2023-09-18 17:37:15 +0200

    remove old files

--- scripts/visualizeClassDiagram.sh
+++
@@ -1,4 +0,0 @@
-#curl -L https://get.pharo.org/64/alpha+vm | bash
-# pharo 11 not ready
-curl -L https://get.pharo.org/100+vm | bash
-./pharo --headless Pharo.image ./scripts/visualizeClassDiagram.st
